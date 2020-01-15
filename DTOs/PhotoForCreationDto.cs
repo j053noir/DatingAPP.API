c4d93929f4d1f60fb524a43183a59f3a82453ec0
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-15 15:46:11 -0500

    Created Photo for creation DTO

---
+++ DTOs/PhotoForCreationDto.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace DatinApp.API.DTOs
+{
+    public class PhotoForCreationDto
+    {
+        public string Url { get; set; }
+        [Required]
+        public IFormFile File { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublicId { get; set; }
+
+        public PhotoForCreationDto()
+        {
+            DateAdded = DateTime.Now;
+        }
+    }
+}
