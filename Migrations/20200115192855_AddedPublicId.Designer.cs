d1c1f5bccd89cb73ed9c100f85b7365c633fa226
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-15 14:33:06 -0500

    Added new property Public Id to Photo

--- Migrations/20200115192855_AddedPublicId.Designer.cs
+++ Migrations/20200115192855_AddedPublicId.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatinApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatinApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200115192855_AddedPublicId")]
+    partial class AddedPublicId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
