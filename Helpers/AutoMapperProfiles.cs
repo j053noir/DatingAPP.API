d5a5ddade014e8a0df5f6c9b1b0bd0a2d7083873
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-16 11:49:26 -0500

    Created user for return DTO

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -27,6 +27,15 @@ namespace DatinApp.API.Helpers
                     dest => dest.Age,
                     opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge())
                 );
+            CreateMap<User, UserForReturnDto>()
+            .ForMember(
+                dest => dest.PhotoUrl,
+                opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url)
+            )
+            .ForMember(
+                dest => dest.Age,
+                opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge())
+            );
             CreateMap<Photo, PhotosForDetailDto>();
             CreateMap<UserForUpdateDto, User>();
             CreateMap<PhotoForCreationDto, Photo>();
