6e17da47f3ffc4b224d998100b9da697cd70f175
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-22 16:17:04 -0500

    Added log user activity filter to scope

--- Startup.cs
+++ Startup.cs
@@ -58,6 +58,7 @@ namespace DatinApp.API
                             ValidateAudience = false
                         };
                     });
+            services.AddScoped<LogUserActivityFilter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
