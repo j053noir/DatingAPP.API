0d7470aa8486d23feea2b7e77be7a45a9080173a
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-22 10:32:21 -0500

    Updated User for registration DTO with new properties

--- DTOs/UserForRegisterDto.cs
+++ DTOs/UserForRegisterDto.cs
@@ -1,8 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace DatinApp.API.DTOs
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "{0} must have length between 4 and 8 characters")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            this.Created = DateTime.Now;
+            this.LastActive = DateTime.Now;
+        }
     }
 }
