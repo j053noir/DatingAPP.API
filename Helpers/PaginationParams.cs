6684771ffe96035b4b629198383e2c66afa15a41
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-23 10:12:12 -0500

    Renamed pagingparams to paginationparams

--- Helpers/PaginationParams.cs
+++ Helpers/PaginationParams.cs
@@ -1,6 +1,6 @@
 namespace DatinApp.API.Helpers
 {
-    public class PagingParams
+    public class PaginationParams
     {
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 10;
