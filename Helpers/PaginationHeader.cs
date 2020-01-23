ed332b21c72749daa4101ef8578c0181b82a748b
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-23 10:10:15 -0500

    Fixed typo in pagination header constructor

--- Helpers/PaginationHeader.cs
+++ Helpers/PaginationHeader.cs
@@ -12,7 +12,7 @@ namespace DatinApp.API.Helpers
             this.CurrentPage = currentPage;
             this.ItemsPerPage = itemsPerPage;
             this.TotalItems = totalItems;
-            this.TotalPages = TotalPages;
+            this.TotalPages = totalPages;
         }
     }
 }
