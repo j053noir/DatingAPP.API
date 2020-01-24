19237375b2f9ce7947280748ab4cdac5a600a3b8
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-24 09:40:16 -0500

    Added orderby and orderdirection to sort records

--- Helpers/PaginationParams.cs
+++ Helpers/PaginationParams.cs
@@ -4,5 +4,7 @@ namespace DatinApp.API.Helpers
     {
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 10;
+        public string OrderBy { get; set; }
+        public string OrderDirection { get; set; } = "descending";
     }
 }
