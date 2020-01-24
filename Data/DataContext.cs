108eff5a50fb42a6c5de6fdfc9a70d6d84295e23
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-24 16:02:31 -0500

    Created Like model, and added it to data context

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -10,5 +10,24 @@ namespace DatinApp.API.Data
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Like>()
+                        .HasKey(k => new { k.LikerId, k.LikeeId });
+
+            modelBuilder.Entity<Like>()
+                        .HasOne(l => l.Likee)
+                        .WithMany(l => l.Likers)
+                        .HasForeignKey(l => l.LikeeId)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Like>()
+                        .HasOne(l => l.Liker)
+                        .WithMany(l => l.Likees)
+                        .HasForeignKey(l => l.LikerId)
+                        .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
---
+++ Models/Like.cs
@@ -0,0 +1,10 @@
+namespace DatinApp.API.Models
+{
+    public class Like
+    {
+        public int LikerId { get; set; }
+        public User Liker { get; set; }
+        public int LikeeId { get; set; }
+        public User Likee { get; set; }
+    }
+}
