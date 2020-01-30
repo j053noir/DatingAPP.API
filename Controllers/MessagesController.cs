cccbbac6956251b70e4f4561343452288ebb7569
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-30 11:44:11 -0500

    Fixed controller base route. Created Created message method.

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -2,7 +2,9 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatinApp.API.Data;
+using DatinApp.API.DTOs;
 using DatinApp.API.Helpers;
+using DatinApp.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,7 +12,7 @@ namespace DatinApp.API.Controllers
 {
     [Authorize]
     [ServiceFilter(typeof(LogUserActivityFilter))]
-    [Route("api/[controller]")]
+    [Route("api/users/{userId}/[controller]")]
     [ApiController]
     public class MessagesController : ControllerBase
     {
@@ -38,6 +40,37 @@ namespace DatinApp.API.Controllers
             }
 
             return Ok(messageFromRepo);
+        }
+
+        [HttpPost(Name = "CreateMessage")]
+        public async Task<IActionResult> CreateMesage(int userId, MessageForCreationDto messageForCreationDto)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Forbid();
+            }
+
+            messageForCreationDto.SenderId = userId;
+
+            var recipient = await this._repo.GetUser(messageForCreationDto.RecipientId);
+
+            if (recipient == null)
+            {
+                return BadRequest("Couldn't find recipient user");
+            }
+
+            var message = this._mapper.Map<Message>(messageForCreationDto);
+
+            this._repo.Add(message);
+
+            if (await this._repo.SaveAll())
+            {
+                var messageToReturn = this._mapper.Map<MessageForCreationDto>(message);
+
+                return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
+            }
+
+            return BadRequest("Failed on saving message");
         }
     }
 }
