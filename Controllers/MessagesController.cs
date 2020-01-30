efe1ebf14d1189169d8e7c339d3edd380e2a6317
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-01-30 16:16:22 -0500

    Created Get message thread endpoint

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -67,6 +67,21 @@ namespace DatinApp.API.Controllers
             return Ok(messageFromRepo);
         }
 
+        [HttpGet("threads/{recipientId}", Name = "GetMessageThread")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Forbid();
+            }
+
+            var messagesFromRepo = await this._repo.GetMessageThread(userId, recipientId);
+
+            var messageThread = this._mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            return Ok(messageThread);
+        }
+
         [HttpPost(Name = "CreateMessage")]
         public async Task<IActionResult> CreateMesage(int userId, MessageForCreationDto messageForCreationDto)
         {
