b1741d22cba3a46cd1d4cbd442e8ee8e0bb685c5
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-02-06 11:00:34 -0500

    Created fallback controller

---
+++ Controllers/FallbackController.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatinApp.API.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(
+                Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "wwwroot/index.html"
+                ),
+                "text/html"
+            );
+        }
+    }
+}
